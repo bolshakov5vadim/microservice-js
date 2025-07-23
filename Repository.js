49211f8317fb10257370869989513bc00985cbbd
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-07-23 18:17:03 +0500

    Repository.js

--- Repository.js
+++ Repository.js
@@ -2,7 +2,7 @@ package com.spring.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.spring.datajpa.model.Employee;
+import com.spring.datajpa.model.Entity;
 
 @Repository
 //Repository берет trycatch+кучу методов (findById...) из JpaRepository
